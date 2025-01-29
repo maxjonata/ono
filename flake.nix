0b8a0e4bb3aa3c9a57f97d69305ff245fb304d3a
JAMaxwell <joaoalfredoff@outlook.com.br>
2025-01-29 03:20:23 -0300

    ci: added codeformat and linting for SOLID principles and clean architecture

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+        nodejs = {
+            url = "github:nodejs/node";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { self, nixpkgs, ... }:
+    outputs = { self, nixpkgs, nodejs, ... }:
     let
         system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };
@@ -15,9 +19,22 @@
                 default = pkgs.mkShell {
                     packages = with pkgs; [
                         rustc cargo rust-analyzer clippy rustfmt rustup # Rust packages
+                        prettier # Adicionar prettier
                     ];
 
-                    shellHook = ''echo "Ono's nix development ambience done"'';
+                    buildInputs = [
+                        nodejs
+                        (pkgs.runCommand "husky" {} ''
+                            mkdir -p $out
+                            cd $out
+                            npm install husky lint-staged --save-dev
+                        '')
+                    ];
+
+                    shellHook = ''
+                        echo "Ono's nix development ambience done"
+                        npm run install:hooks
+                    '';
                 };
             };
         };
