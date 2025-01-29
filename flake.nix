8bb18c492c2a2c75c8443cf83b823d9bd29c9e4e
JAMaxwell <joaoalfredoff@outlook.com.br>
2025-01-29 02:05:59 -0300

    flake configured

--- flake.nix
+++ flake.nix
@@ -5,17 +5,23 @@
         nixpkgs.url = "github:NixOS/nixpkgs/23.05";
     };
 
-    outputs = { self, nixpkgs, }: {
-        devShells.default = nixpkgs.lib.mkShell {
-            packages = with nixpkgs; [
-                git  # Version control
-                rustc cargo rust-analyzer clippy rustfmt rustup # Rust packages
-                direnv nix-direnv # Nix autorun configuration files
-            ];
+    outputs = { self, nixpkgs, ... }:
+    let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+    in {
+        devShells = {
+            x86_64-linux = {
+                default = pkgs.mkShell {
+                    packages = with pkgs; [
+                        git  # Version control
+                        rustc cargo rust-analyzer clippy rustfmt rustup # Rust packages
+                        direnv nix-direnv # Nix autorun configuration files
+                    ];
 
-            shellHook = ''
-                eval "$(direnv hook bash)" && echo "Ono's nix development ambience done"
-            '';
+                    shellHook = ''eval "$(direnv hook bash)" && echo "Ono's nix development ambience done"'';
+                };
+            };
         };
     };
 }
